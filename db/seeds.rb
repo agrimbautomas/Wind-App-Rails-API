69a6bb732a148a105a3df8bc34fa72df2dfb20df
Tomas Agrimbau <tomas@amalgama.co>
2019-06-18 18:47:11 -0300

    Add seeds default

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ if Rails.env.development?
 	User.create!(email: 'john@doe.com', password: 'password', password_confirmation: 'password')
 	User.create!(email: 'anon@user.com', password: 'password', password_confirmation: 'password')
 
-	AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+	AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 	Station.create!(name: 'Carmelo')
 	Station.create!(name: 'Colonia')
