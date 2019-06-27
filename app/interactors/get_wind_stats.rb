c9353ef520a249725889af655435d3d6fe58a6f1
Tomas Agrimbau <tomas@amalgama.co>
2019-06-27 11:51:41 -0300

    Add default method to get_wind_stats

--- app/interactors/get_wind_stats.rb
+++ app/interactors/get_wind_stats.rb
@@ -1,5 +1,13 @@
 class GetWindStats < Interactor
 
+	def self.default
+		get_wind_stats = new
+		get_wind_stats.execute
+	end
+
+	def initialize
+	end
+
 	def execute
 		Station.find_by_slug('colonia').wind_logs.limit(3).order(registered_date: :desc)
 	end
