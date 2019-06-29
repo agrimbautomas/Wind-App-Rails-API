c5b4306de6da652448a05a4a22b07a6d1adbcb3a
Tomas Agrimbau <tomas@amalgama.co>
2019-06-29 10:44:02 -0300

    Change to new domain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
-role :app, %w{deploy@wind-api.amalgama.co}
-role :web, %w{deploy@wind-api.amalgama.co}
-role :db,  %w{deploy@wind-api.amalgama.co}
+role :app, %w{deploy@api.vientoenelrio.com}
+role :web, %w{deploy@api.vientoenelrio.com}
+role :db,  %w{deploy@api.vientoenelrio.com}
 
 set :deploy_to, '/srv/rails/wind-api'
 
-server 'wind-api.amalgama.co', user: 'deploy', roles: %w{web app}
+server 'api.vientoenelrio.com', user: 'deploy', roles: %w{web app}
 set :rails_env, 'production'
 set :branch, 'production'
