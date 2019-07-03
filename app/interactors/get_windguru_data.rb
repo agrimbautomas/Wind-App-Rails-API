3afed0744b3b27acc7d286b74a5fa3caef0a70cf
Tomas Agrimbau <tomas@amalgama.co>
2019-07-03 16:15:41 -0300

    Add method to iterate intents in windguru api

--- app/interactors/get_windguru_data.rb
+++ app/interactors/get_windguru_data.rb
@@ -8,14 +8,25 @@ class GetWindguruData < Interactor
 
 	def initialize(endpoint:)
 		@endpoint = endpoint
+		@intent_counter = 0
 	end
 
 	def execute
-		response = GetRequest.to(uri: uri, params: params, headers: headers)
-		@logs = clean_response response
+		@logs = get_wind_logs
 	end
 
 	private
+
+	def get_wind_logs
+		3.times do
+			@response = GetRequest.to(uri: uri, params: params, headers: headers)
+			break if @response['fcst'].present?
+
+			@intent_counter += 1
+		end
+
+		clean_response @response
+	end
 
 	def uri
 		'https://www.windguru.net/int/' + @endpoint
@@ -24,12 +35,25 @@ class GetWindguruData < Interactor
 	def params
 		query = '?q=forecast'
 		query += '&id_model=29'
-		#query += '&initstr=' + DateTime.now.strftime('%Y%m%d') + '06'
-		query += '&initstr=2019070118'
+
+		query += '&initstr=' + initstr(@intent_counter)
 		query += '&id_spot=261'
 		query += '&WGCACHEABLE=21600'
 		query += '&cachefix=-34.58x-58.4x30'
 		query
+	end
+
+	def initstr intent
+		# Four expected conbinations of the code,
+		# from more probable to less probable
+		case intent
+		when 0
+			DateTime.yesterday.strftime('%Y%m%d') + '18'
+		when 1
+			DateTime.now.strftime('%Y%m%d') + '06'
+		else
+			DateTime.now.strftime('%Y%m%d') + '18'
+		end
 	end
 
 	def headers
